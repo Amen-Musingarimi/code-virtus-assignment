a7824eb864fdd8310eb76a0f6177f0eaf4df7594
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-15 10:41:14 +0200

    Add employer details component

--- src/app/employer-details/employer-details.component.ts
+++ src/app/employer-details/employer-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-employer-details',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './employer-details.component.css'
 })
 export class EmployerDetailsComponent {
+  @Input() formData: any;
+  @Output() nextStep = new EventEmitter<void>();
+  @Output() prevStep = new EventEmitter<void>();
+  @Output() handleChange = new EventEmitter<{ field: string; value: any }>();
 
+  errorMessage: string = '';
+
+  handleNext(): void {
+    this.nextStep.emit();
+  }
+
+  handlePrev(): void {
+    this.prevStep.emit();
+  }
 }
