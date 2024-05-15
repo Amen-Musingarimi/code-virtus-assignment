8068c81fd1b738ccb4a00b246f2f4c55f4433e2c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-15 12:04:45 +0200

    Add title from the backent

--- src/app/form-data.service.ts
+++ src/app/form-data.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Client } from './client.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,18 @@ export class FormDataService {
       'https://api.clientsure.codevirtus.com/opn/v1/individual-client/create',
       formData
     );
+  }
+
+  fetchTitles(): Observable<{ id: number; name: string }[]> {
+    return this.http
+      .get<any>('https://api.clientsure.codevirtus.com/opn/v1/titles/all')
+      .pipe(
+        map((response: any) =>
+          response.content.map((title: any) => ({
+            id: title.id,
+            name: title.name
+          }))
+        )
+      );
   }
 }
--- src/app/personal-data/personal-data.component.ts
+++ src/app/personal-data/personal-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormDataService } from '../form-data.service';
 
 @Component({
   selector: 'app-personal-data',
@@ -9,6 +10,30 @@ export class PersonalDataComponent {
   @Input() formData: any;
   @Output() nextStep = new EventEmitter<void>();
   @Output() handleChange = new EventEmitter<{ field: string; value: any }>();
+
+  titles: { id: number; name: string }[] = [];
+
+  constructor(private formDataService: FormDataService) {}
+
+  ngOnInit(): void {
+    this.fetchTitles();
+  }
+
+  fetchTitles(): void {
+    this.formDataService.fetchTitles().subscribe(
+      titles => {
+        this.titles = titles;
+      },
+      error => {
+        console.error('Error fetching titles:', error);
+      }
+    );
+  }
+
+  handleTitleChange(event: any): void {
+    const selectedTitleId = event.target.value;
+    this.handleChange.emit({ field: 'title', value: selectedTitleId });
+  }
 
   errorMessage: string = '';
   addressValid: boolean = false;
